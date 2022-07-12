25004255876a0e7ee498416b2f6b1d55a94fd1a3
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-07-12 11:19:28 +0100

    Update quiz.sql

--- #banco_de_dados/quiz.sql
+++ #banco_de_dados/quiz.sql
@@ -61,27 +61,26 @@ INSERT INTO `perguntas` (`id`, `pergunta`, `A`, `B`, `C`, `D`, `resposta_certa`,
 (13, 'Como se chama, nos humanos, a bolsa onde se acumula a urina?', 'Rins', 'Vesicula', 'Utera', 'Bexiga(certa)', 'D', 1, '1'),
 (14, 'Quantos dias tem um ano Bissexto?', '354', '366(certa)', '456', '365', 'B', 1, '1'),
 (15, 'Quem foi Neil Armstrong?', 'Astronauta(certa)', 'Bombeiro', 'Tenista', 'Camionista', 'A', 1, '1'),
-(16, 'Que nome se dÃ¡ Ã  agulha que indica as horas num relÃ³gio', 'Indicador', 'Seta', 'Alfinete', 'Ponteiro(certa)', 'D', 1, '1'),
-(17, 'Como se chama a filha mais velha da sÃ©rie \"Os simpsons\"?', 'Marge', 'Maggie', 'Lisa(certa)', 'Bart', 'C', 1, '1'),
-(18, 'Qual dos seguintes representa um operador lÃ³gico?', 'SubtraÃ§Ã£o', 'Soma', 'DivisÃ£o', 'XOR(certa)', 'D', 1, '1'),
-(19, 'Que nome se dÃ¡ ao estudo do sistema imunitÃ¡rio?', 'Dermatologia', 'Hematologia', 'Anatomia', 'Imunologia(certa)', 'D', 1, '1'),
-(20, 'Que nome se dÃ¡ a um Ã¢ngulo de 90 graus?', 'Reto(certa)', 'Obtuso', 'Raso', 'Agudo', 'A', 1, '1'),
-(21, 'Qual Ã© a sÃ©tima arte?', 'Teatro', 'Pintura', 'Cinema(certa)', 'DanÃ§a', 'C', 1, '3'),
-(22, 'Qual dos seguintes nÃ£o faz fronteira com Brasil?', 'BolÃ­via', 'Peru', 'Paraguai', 'Chile(certa)', 'D', 1, '3'),
-(23, 'A que cantora pertence o Ã¡lbum \"Good girl gone bad\"?', 'Jennifer Lopes', 'Amy Winehouse', 'Lady Gaga', 'Rihana(certa)', 'D', 2, '3'),
-(24, 'Em que paÃ­s se realizou o EURO 2004?', 'Espanha', 'Portugal(certa)', 'Turquia', 'Holanda', 'B', 2, '3'),
-(25, 'Qual Ã© a nacionalidade do estilista Giorgio Armani?', 'AlemÃ£o', 'FrancÃªs', 'Italiano(certa)', 'PortuguÃªs', 'C', 2, '3'),
-(26, 'Qual das seguintes mÃºsicas pertence aos \"Linkin park\"?', 'Master of Puppets', 'Last Resort', 'Burn it Down(certa)', 'Killing in the Name', 'C', 2, '2'),
-(27, 'Qual dos seguintes nÃ£o Ã© um web Browser?', 'Opera', 'Firefox', 'Thunderbird(certa)', 'Safari', 'C', 2, '3'),
+(16, 'Que nome se dá à agulha que indica as horas num relógio', 'Indicador', 'Seta', 'Alfinete', 'Ponteiro(certa)', 'D', 1, '1'),
+(17, 'Como se chama a filha mais velha da série \"Os simpsons\"?', 'Marge', 'Maggie', 'Lisa(certa)', 'Bart', 'C', 1, '1'),
+(18, 'Qual dos seguintes representa um operador lógico?', 'Subtração', 'Soma', 'Divisão', 'XOR(certa)', 'D', 1, '1'),
+(19, 'Que nome se dá ao estudo do sistema imunitário?', 'Dermatologia', 'Hematologia', 'Anatomia', 'Imunologia(certa)', 'D', 1, '1'),
+(20, 'Que nome se dá a um ângulo de 90 graus?', 'Reto(certa)', 'Obtuso', 'Raso', 'Agudo', 'A', 1, '1'),
+(21, 'Qual é a sétima arte?', 'Teatro', 'Pintura', 'Cinema(certa)', 'Dança', 'C', 1, '3'),
+(22, 'Qual dos seguintes não faz fronteira com Brasil?', 'Bolívia', 'Peru', 'Paraguai', 'Chile(certa)', 'D', 1, '3'),
+(23, 'A que cantora pertence o álbum \"Good girl gone bad\"?', 'Jennifer Lopes', 'Amy Winehouse', 'Lady Gaga', 'Rihana(certa)', 'D', 2, '3'),
+(24, 'Em que país se realizou o EURO 2004?', 'Espanha', 'Portugal(certa)', 'Turquia', 'Holanda', 'B', 2, '3'),
+(25, 'Qual é a nacionalidade do estilista Giorgio Armani?', 'Alemão', 'Francês', 'Italiano(certa)', 'Português', 'C', 2, '3'),
+(26, 'Qual das seguintes músicas pertence aos \"Linkin park\"?', 'Master of Puppets', 'Last Resort', 'Burn it Down(certa)', 'Killing in the Name', 'C', 2, '2'),
+(27, 'Qual dos seguintes não é um web Browser?', 'Opera', 'Firefox', 'Thunderbird(certa)', 'Safari', 'C', 2, '3'),
 (28, 'Quem foi o vocalista dos \"Nirvana?\"', 'Axl Rose', 'Kurt Cobain(certa)', 'Dave Grohl', 'Frank Sinatra', 'B', 2, '2'),
-(29, 'A que cantora pertence a mÃºsica \"Skyscraper\"?', 'Miley Cyrus', 'Demi Lovato(certa)', 'Adele', 'Katy Perry', 'B', 2, '3'),
-(30, 'A estaÃ§Ã£o das chuvas curtas em Angola vai de...', 'Fevereiro Ã  Abril(certa)', 'Dezembro Ã  MarÃ§o', 'Janeiro Ã  Junho', 'Agosto Ã  Janeiro', 'A', 2, '3'),
-(31, 'De que provÃ­ncia Ã© primeira Miss Angola?', 'BiÃ©', 'Luanda', 'Benguela', 'Cabinda(certa)', 'D', 2, '3'),
-(32, 'Qual dos paÃ­ses nÃ£o faz fronteira com Angola?', 'ZÃ¢mbia', 'NamÃ­bia', 'Congo', 'Ãfrica do Sul(certa)', 'D', 2, '3'),
+(29, 'A que cantora pertence a música \"Skyscraper\"?', 'Miley Cyrus', 'Demi Lovato(certa)', 'Adele', 'Katy Perry', 'B', 2, '3'),
+(30, 'A estação das chuvas curtas em Angola vai de...', 'Fevereiro à Abril(certa)', 'Dezembro à Março', 'Janeiro à Junho', 'Agosto à Janeiro', 'A', 2, '3'),
+(31, 'De que província é primeira Miss Angola?', 'Bié', 'Luanda', 'Benguela', 'Cabinda(certa)', 'D', 2, '3'),
+(32, 'Qual dos países não faz fronteira com Angola?', 'Zâmbia', 'Namíbia', 'Congo', 'África do Sul(certa)', 'D', 2, '3'),
 (33, 'O atentado de 11 de setembro aconteceu em que ano?', '2001(certa)', '2008', '1994', '1901', 'A', 3, '1'),
-(34, 'Em que paÃ­s se iniciou a revoluÃ§Ã£o industrial?', 'Estados Unidos da Ãmerica', 'Alemanha', 'Portugal', 'Inglaterra(certa)', 'D', 3, '1'),
-(35, 'Quem criou este aplicativo?', 'Afonso Dumbo', 'Isaac GlÃ³ria', 'Francisco Fetapi(certa)', 'Francisco dos Santos', 'C', 3, '1'),
-(36, 'ffgdgfgfhgfhfhfhhhghghghfhfhffhfhghhghghghhhghghghghghghghghghghghhghghghghhghghghhghgh', 'ffffffff', 'ssssss', 'vvvvv(certa)', 'ttttt', 'C', 1, '3');
+(34, 'Em que país se iniciou a revolução industrial?', 'Estados Unidos da Ámerica', 'Alemanha', 'Portugal', 'Inglaterra(certa)', 'D', 3, '1'),
+(35, 'Quem criou este aplicativo?', 'Afonso Dumbo', 'Isaac Glória', 'Francisco Fetapi(certa)', 'Francisco dos Santos', 'C', 3, '1');
 
 
 
@@ -95,8 +94,8 @@ CREATE TABLE `usuario` (
 
 
 INSERT INTO `usuario` (`id`, `nome`, `senha`, `pontos`, `critica`) VALUES
-(10, 'Francisco Fetapi', '      ', 32880, 'Gostei da aplicaÃ§Ã£o, tem uma jogabilidade Ã³tima!'),
-(11, 'Carlos Mateus', '      ', 1200, 'muito bom, sÃ³ achei que devia ter uma forma de demonstrar o carregamento do dados nos modais, tipo uma barra de progresso ou aqueles circulos que ficam girando enquanto os dados sÃ£o carregados, alÃ©m disso, estÃ¡ Ã³timo!'),
+(10, 'Francisco Fetapi', '      ', 32880, 'Gostei da aplicação, tem uma jogabilidade ótima!'),
+(11, 'Carlos Mateus', '      ', 1200, 'muito bom, só achei que devia ter uma forma de demonstrar o carregamento do dados nos modais, tipo uma barra de progresso ou aqueles circulos que ficam girando enquanto os dados são carregados, além disso, está ótimo!'),
 (12, 'Maria Eduardo', '      ', 13550, 'Legal, gostei do fato de eu puder escolher qual musica quero para a abertura!'),
 (13, 'Usuario Novo', '      ', 450, '');
 
